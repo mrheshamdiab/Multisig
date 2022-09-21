ed58f64a39469ae901c8b907f0a26e42ad342fce
mrheshamdiab <102466832+mrheshamdiab@users.noreply.github.com>
2022-09-21 19:53:36 +0300

    Add files via upload

---
+++ build/precache-manifest.63b565b16b68fc5aa2c450761778659d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599e06513288c693eec3ea17b09d006c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "820528ba241919c99165",
+    "url": "./static/css/2.f44a53cc.chunk.css"
+  },
+  {
+    "revision": "0fbe5600c224ac5ae091",
+    "url": "./static/css/main.096a0c0b.chunk.css"
+  },
+  {
+    "revision": "820528ba241919c99165",
+    "url": "./static/js/2.532ee092.chunk.js"
+  },
+  {
+    "revision": "53d78e7234fe6cf58dc23125b52ff5c1",
+    "url": "./static/js/2.532ee092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbe5600c224ac5ae091",
+    "url": "./static/js/main.f229d6d5.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
